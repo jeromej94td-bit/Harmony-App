2575355c42ed14fae1f7f0a43d7bb670a706c4ec
jeromej94td-bit <jeromej94td@gmail.com>
2026-08-10 14:03:32 +0200

    Add complete regional Spanish localization

--- app/src/main/java/com/example/ui/TranslationCatalog.kt
+++ app/src/main/java/com/example/ui/TranslationCatalog.kt
@@ -15,7 +15,15 @@ internal object TranslationCatalog {
 
     private val packs: Map<String, LocalePack> = mapOf(
         "en" to localePack(EXACT_ENGLISH_CONTENT, ::localizeEnglishDynamicContent),
-        "it" to localePack(EXACT_ITALIAN_CONTENT, ::localizeItalianDynamicContent)
+        "it" to localePack(EXACT_ITALIAN_CONTENT, ::localizeItalianDynamicContent),
+        "es-419" to localePack(
+            EXACT_SPANISH_LATIN_AMERICA_CONTENT,
+            ::localizeLatinAmericanSpanishDynamicContent
+        ),
+        "es-ES" to localePack(
+            EXACT_SPANISH_SPAIN_CONTENT,
+            ::localizeSpainSpanishDynamicContent
+        )
     )
 
     private fun localePack(
